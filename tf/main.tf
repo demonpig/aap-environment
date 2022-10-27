d092e57eaf2e30fe069b2394d97bb23fdaa15ec0
Max Mitschke <maxmitschke@fastmail.com>
2022-10-27 09:58:09 -0400

    Upgrading the system specs for openstack for aap

--- tf/main.tf
+++ tf/main.tf
@@ -11,30 +11,14 @@ required_version = ">= 0.14.0"
 
 # Ansible Automation Platform Environment
 # Defaults
-#   - 1 RHEL 8 Main (used to SSH into to install AAP)
 #   - 1 RHEL 8 Automation Controller
 #   - 1 RHEL 8 Execution Environment
 #   - 1 RHEL 8 Automation Hub
-resource "openstack_compute_instance_v2" "main" {
-  name = "${var.username}_aap_${var.aap_version}_main"
-  image_name = "RHEL-8.6.0-x86_64-released"
-  flavor_name = "ci.m1.micro"
-  key_pair = "${var.username}-rsa"
-  security_groups = ["default"]
-  
-  network {
-      name = "provider_net_shared"
-  }
-
-  metadata = {
-    description = "Used for SSHing into to install AAP on the other servers"
-  }
-}
 
 resource "openstack_compute_instance_v2" "controller" {
   name = "${var.username}_aap_${var.aap_version}_controller"
   image_name = "RHEL-8.6.0-x86_64-released"
-  flavor_name = "m1.medium"
+  flavor_name = "g.memory.medium"
   key_pair = "${var.username}-rsa"
   security_groups = ["default"]
   
@@ -50,7 +34,7 @@ resource "openstack_compute_instance_v2" "controller" {
 resource "openstack_compute_instance_v2" "automation_hub" {
   name = "${var.username}_aap_${var.aap_version}_automation_hub"
   image_name = "RHEL-8.6.0-x86_64-released"
-  flavor_name = "m1.medium"
+  flavor_name = "g.memory.medium"
   key_pair = "${var.username}-rsa"
   security_groups = ["default"]
   
